8494db8d0928afc52f286f2767d4aef70a9e6cb2
JosueDurand96 <josue.durand8@gmail.com>
2019-10-27 17:33:10 -0500

    a

--- app/src/main/java/com/codepath/mypizza/MainActivity.java
+++ app/src/main/java/com/codepath/mypizza/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity  implements PizzaMenuFragmen
       args.putInt("position", 0);
       secondFragment.setArguments(args);          // (1) Communicate with Fragment using Bundle
       FragmentTransaction ft2 = getSupportFragmentManager().beginTransaction();// begin  FragmentTransaction
-      ft2.add(R.id.flContainer2, secondFragment);                               // add    Fragment
+     // ft2.add(R.id.flContainer2, secondFragment);                               // add    Fragment
       ft2.commit();                                                            // commit FragmentTransaction
     }
   }
@@ -52,19 +52,10 @@ public class MainActivity extends AppCompatActivity  implements PizzaMenuFragmen
     args.putInt("position", position);
     secondFragment.setArguments(args);          // (1) Communicate with Fragment using Bundle
 
-
-    if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
-      getSupportFragmentManager()
-          .beginTransaction()
-          .replace(R.id.flContainer2, secondFragment) // replace flContainer
-          //.addToBackStack(null)
-          .commit();
-    }else{
-      getSupportFragmentManager()
-          .beginTransaction()
-          .replace(R.id.flContainer, secondFragment) // replace flContainer
-          .addToBackStack(null)
-          .commit();
-    }
+    getSupportFragmentManager()
+            .beginTransaction()
+            .replace(R.id.flContainer, secondFragment) // replace flContainer
+            .addToBackStack(null)
+            .commit();
   }
 }
